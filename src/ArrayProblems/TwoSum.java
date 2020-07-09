0d7d30e34a9434ca3ad2f314cd93584b924fd6e4
Barry Cheng <barrycheng8@gmail.com>
2020-07-09 10:10:30 -0700

    fix merge conflicts

--- src/ArrayProblems/TwoSum.java
+++ src/ArrayProblems/TwoSum.java
@@ -2,13 +2,21 @@
 // Given an array of integers, return the indices of the numbers that add up to the specific target
 // There will only be exactly 1 solution, and the element in the same index may not be used twice
 // Ex. [2, 7, 11, 15], target 9: return [0, 1] (2 + 7 = 9)
-// Intuition: 
+// Intuition: The operands in a sum can always be switched around, i.e. 2 + 7 = 7 + 2
+// Using this idea, we traverse through the array scanning the map for a number that complements the current num to reach the target.
+// If no such number exists, we add it to the map, mapping num to index. Once we find a matching pair, the index
+// found with the map will always be less than the current index. therefore we set ans to [target-nums[i] index, curr index]
+//
+// O(n) runtime: worst case we traverse through the entire array once
+// O(n) space: worst case we traverse through the array and add all elements to the map without obtaining an answer
 
+package ArrayProblems;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
 
+    // This can also be solved by sorting and using 2 pointers
     public static int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         // {value: position in array}
