a16e2fbbaa3c6abc4613b256698b194f2e40578f
Barry Cheng <barrycheng8@gmail.com>
2020-07-30 14:00:14 -0700

    july 30

--- src/DP/BestTimeToBuyAndSellStockCooldown.java
+++ src/DP/BestTimeToBuyAndSellStockCooldown.java
@@ -1,8 +1,47 @@
+// LeetCode 309. Best Time to Buy and Sell Stock with Cooldown
+// Given an array of prices, where the i-th element is the price on a given day
+// You can buy and sell as many times as you want, but you cannot perform multiple transactions on the same day (buy and sell on the same day)
+// After you sell a stock, you cannot buy on the next day (1 day cooldown)
+// Ex. [1, 2, 3, 0, 2] maxProfit = 3 , [buy, sell, rest, buy, sell]
+//
+// Intuition: This is a DP problem. Essentially we have 3 states:
+// *** NEED TO REVISIT THIS PROBLEM! 7/29/20 ***
+
 package DP;
 
 public class BestTimeToBuyAndSellStockCooldown {
 
+    // Another method:
+    // dp[i][0]: not holding any stock on day i (rest or sold)
+    // dp[i][1]: bought stock on day i
+    // Case 1: Bought stock on day i, dp[i][1] is the max of:
+    // - I bought it today
+    //   dp[i-2][0] - prices[i] take best price before cool down after selling stock
+    // - I am holding the stock
+    //   dp[i-1][1]   I am still holding the stock so my profit remains the same as the previous day
+    // Case 2: We don't have stock on day i. d[i][0] is the max of:
+    // - I sold it today
+    //   dp[i-1][1] + prices[i] we take the previous best profit and add our current selling price to the profit
+    // - I am holding the stock
+    //   dp[i-1][0]
     public int maxProfit(int[] prices) {
+        if(prices.length < 2) return 0;
+        int len = prices.length;
 
+        int[] s0 = new int[len]; // resting and can buy stock
+        int[] s1 = new int[len]; // holding stock, can sell
+        int[] s2 = new int[len]; // sold the stock, must rest next day
+
+        s0[0] = 0; // we start off with no stock
+        s1[0] = -prices[0]; // if we buy the stock on this day, this is our total cost
+        s2[0] = Integer.MIN_VALUE; // profit base case. selling the stock at any time will beat this initial case
+
+        for (int i = 1; i < len; i++) {
+            s0[i] = Math.max(s0[i - 1], s2[i - 1]); // we continue to hold no stock or we recover from cooldown
+            s1[i] = Math.max(s1[i - 1], s0[i - 1] - prices[i]); // Compare buying price today and yesterday. Choose the better value
+            s2[i] = s1[i - 1] + prices[i]; // Use yesterdays buying price and see how much profit we get if we sold today
+        }
+
+        return Math.max(s0[len - 1], s2[len - 1]);
     }
 }
---
+++ src/DP/WordBreakII.java
@@ -0,0 +1,102 @@
+/*
+ LeetCode 140. Word Break II
+ Given a non-empty string s and a dictionary wordDict that contains a list of non-empty words, add spaces between s
+ to construct a sentence where each word is a valid dictionary word. Return all possible sentences
+ The same word can be reused in multiple sentences. The dictionary will not contain duplicate words
+
+ Example:
+ s = "catsanddog"         wordDict = ["cat", "cats", "and", "sand", "dog"]
+ Output: [ "cats and dog", "cat sand dog"]
+
+ Intuition:
+ To find all possible sentences, we need to break down the original string into smaller substrings
+ We then recurse and find the answer to these smaller substrings. After each recursive call, we build up the final result
+ which we pass back up the previous function call. We then add the word in front of the answer to the smaller substring
+ to build the final sentence
+
+ Visual Representation:
+ pineapplepenapple
+
+ *** This actually breaks into two cases. Creating an answer with "apple" and another with "applepen" ***
+ pine -> wordBreak(applepenapple)
+ pine -> apple -> wordBreak(penapple)
+ pine -> apple -> pen -> wordBreak(apple)
+ pine -> apple -> pen -> apple -> wordBreak("") Base Case -> return and build the sentence
+ pine -> apple -> pen -> "apple"
+ pine -> apple -> "pen apple"
+ pine -> "apple pen apple"
+ "pine apple pen apple"
+
+ pine -> applepen -> wordBreak(apple)
+ pine -> applepen -> apple -> wordBreak("") Base Case -> return and rebuild the sentence
+ pine -> applepen -> "apple"
+ pine -> "applepen apple"
+ "pine apple pen apple"
+
+ pineapple -> wordBreak(penapple)
+ pineapple -> pen -> wordBreak(apple)
+ pineapple -> pen -> apple -> wordBreak("") Base Case -> return and rebuild the sentence
+ pineapple -> pen -> "apple"
+ pineapple -> "pen apple"
+ "pineapple pen apple"
+
+ O(2^n) runtime: In the worst case, s="aaa" and wordDict=["a", "aa", "aaa"]. Each partition results in a possible sentence,
+                 thus creating 2^n-1 partitions
+                 Ex. ["a a a", "a aa", "aa a", "aaa"] 2^(3-1)=4
+                 However, this does optimize against the s="aaab" case, where it figures out after 1 full recursive call
+                 that the substring including "b" does not lead to any valid solutions
+ O(2^n) space: In the worst case, we have passed in parameters similar to above, which would lead to 2^(n-1) solutions.
+*/
+
+package DP;
+
+import java.util.*;
+
+public class WordBreakII {
+
+    public static List<String> wordBreak(String s, List<String> wordDict) {
+        return wordBreakHelper(s, wordDict, new HashMap<>());
+    }
+
+    public static List<String> wordBreakHelper(String s, List<String> wordDict, Map<String, List<String>> memo) {
+        // DP: use previous result if we already solved it
+        if (memo.containsKey(s))
+            memo.get(s);
+
+        List<String> res = new ArrayList<>();
+
+        // Base case if string is empty
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+
+        // If this loop completes without any recursion, we know the substring does not contain any valid words
+        // from wordDict. In that scenario we return an empty list.
+        for (String word : wordDict) {
+            // Check if the substring begins with any of the words in the dictionary
+            if (s.startsWith(word)) {
+                // Recurse on the substring that removes the word found in the dictionary
+                List<String> subStrings = wordBreakHelper(s.substring(word.length()), wordDict, memo);
+
+                // Add the sentences created by each substring to the result and return it to the function call a level above
+                for (String sub : subStrings) {
+                    // Check if this is the first time we are adding the sentence. If it is, we do not include a space before it
+                    String optionalSpace = (sub.length() == 0) ? "" : " ";
+                    res.add(word + optionalSpace + sub);
+                }
+            }
+        }
+
+        // Memoize the results for each substring. This will improve overall runtime
+        memo.put(s, res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "catsanddog";
+        List<String> wordDict = new ArrayList<>(Arrays.asList("cat", "cats", "and", "sand", "dog"));
+
+        System.out.println(wordBreak(s, wordDict));
+    }
+}
